be910311bb7f4308ffc814c3f6e6f4c59c07fe8a
Andrea Mancini <mancioman2@gmail.com>
2021-11-08 18:41:21 +0100

    port as int fix

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ ftp = FTP()
 
 path = sys.argv[1]
 host = sys.argv[2]
-port = sys.argv[3]
+port = int(sys.argv[3])
 user = sys.argv[4]
 pwd = sys.argv[5]
 
@@ -15,9 +15,11 @@ pwd = sys.argv[5]
 def run_program():
     print('I start to build your react app')
     react.build_react(path)
-    print('react built complete')
+    print('connect to: ' + host)
     ftpcom.ftp_connect(ftp, host, port, user, pwd)
+    print('remove static folder')
     ftpcom.ftp_remove_dir(ftp, 'static')
+    print('upload files')
     ftpcom.ftp_upload(ftp, path + '\\build')
     ftp.quit()
 
